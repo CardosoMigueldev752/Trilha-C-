8e5c9b4c52c7cfb0bdc48d2eb4c5fe066ab1cb1b
CardosoMiguel <cardosomiguel.contatopro@gmail.com>
2026-07-24 14:10:59 -0300

    Add files via upload

---
+++ exercicio contador/exercicio contador/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.SetCursorPosition(10, 4);
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Meu");
+System.Threading.Thread.Sleep(1000);
+Console.ResetColor();
+
+Console.SetCursorPosition(10, 5);
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Brasil");
+System.Threading.Thread.Sleep(1000);
+Console.ResetColor();
+
+Console.SetCursorPosition(10, 6);
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Brasileiro");
+System.Threading.Thread.Sleep(1000);
+Console.ResetColor();
